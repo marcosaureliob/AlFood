be9060045bf046c8c7d79ac39ac62a6f71a8fbf8
Marcos Aurelio <77805407+marcosaureliob@users.noreply.github.com>
2023-08-03 23:48:56 -0300

    Finalização de projeto

--- src/paginas/Administracao/Restaurantes/AdministracaoRestaurantes.tsx
+++ src/paginas/Administracao/Restaurantes/AdministracaoRestaurantes.tsx
@@ -1,39 +1,71 @@
 import { useEffect, useState } from "react";
 import IRestaurante from "../../../interfaces/IRestaurante";
-import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
-import axios from "axios";
+import {
+  Button,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+
+} from "@mui/material";
+import { Link as RouterLink } from "react-router-dom";
+import http from "../../../http";
 
 const AdministracaoRestaurantes = () => {
   const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([]);
 
-    useEffect(() => {
-        axios.get<IRestaurante[]>('http://localhost:8000/api/v2/restaurantes/')
-        .then(resposta => setRestaurantes(resposta.data))
-    })
+  useEffect(() => {
+    http
+      .get<IRestaurante[]>("/restaurantes/")
+      .then((resposta) => setRestaurantes(resposta.data));
+  });
+
+  const excluir = (restauranteAhSerExcluido: IRestaurante) => {
+    http.delete(`/restaurantes/${restauranteAhSerExcluido.id}/`).then(() => {
+      const listaRestaurante = restaurantes.filter(
+        (restaurante) => restaurante.id !== restauranteAhSerExcluido.id
+      );
+      setRestaurantes([...listaRestaurante]);
+    });
+  };
 
   return (
     <div>
       <TableContainer component={Paper}>
         <Table>
-            <TableHead>
-                <TableRow>
-                    <TableCell>
-                        Nome
-                    </TableCell>
-                </TableRow>
-            </TableHead>
-            <TableBody>
-                {restaurantes.map(restaurante => <TableRow key={restaurante.id}>
-                    <TableCell>
-                        {restaurante.nome}
-                    </TableCell>
-                </TableRow>)}
-                <TableRow>
-                    <TableCell>
-                        Nome
-                    </TableCell>
-                </TableRow>
-            </TableBody>
+          <TableHead>
+            <TableRow>
+              <TableCell>Nome</TableCell>
+              <TableCell>Editar</TableCell>
+              <TableCell>Excluir</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {restaurantes.map((restaurante) => (
+              <TableRow key={restaurante.id}>
+                <TableCell>{restaurante.nome}</TableCell>
+                <TableCell>
+                  [
+                  <RouterLink to={`/admin/restaurantes/${restaurante.id}`}>
+                    Editar
+                  </RouterLink>
+                  ]
+                </TableCell>
+                <TableCell>
+                  <Button
+                    variant="outlined"
+                    color="error"
+                    onClick={() => excluir(restaurante)}
+                  >
+                    Excluir
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
         </Table>
       </TableContainer>
     </div>
